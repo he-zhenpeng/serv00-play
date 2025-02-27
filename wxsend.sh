e38ee4b9960ddea42796ae292a98c628b2c17081
he-zhenpeng <104309499+he-zhenpeng@users.noreply.github.com>
2025-02-27 13:45:42 +0800

    Update wxsend.sh

--- wxsend.sh
+++ wxsend.sh
@@ -5,11 +5,7 @@ text=$1
 sendKey=${WXSENDEKEY}
 title="msg_from_serv00-play"
 URL="https://sctapi.ftqq.com/$sendKey.send?"
-
-if [[ -z ${sendKey} ]]; then
-  echo "未配置微信推送的sendKey,通过 https://sct.ftqq.com/r/13223 注册并登录server酱，取得sendKey"
-else
-  res=$(timeout 20s curl -s -X POST $URL -d title=${title} -d desp="${text}")
+res=$(timeout 20s curl -s -X POST https://5742.push.ft07.com/send/sctp5742t93u1b5cumq82fnkmdleu7p.send? -d title=${title} -d desp="${text}")
   if [ $? == 124 ]; then
     echo "发送消息超时"
     exit 1
@@ -21,4 +17,3 @@ else
   else
     echo "微信推送失败, error:$err"
   fi
-fi
