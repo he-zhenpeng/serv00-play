0e5e6be191aa50ed8d18b36f27ca88b34c2b85b0
he-zhenpeng <104309499+he-zhenpeng@users.noreply.github.com>
2025-02-27 14:52:24 +0800

    Update wxsend.sh

--- wxsend.sh
+++ wxsend.sh
@@ -41,36 +41,12 @@ formatted_msg=$(echo "$result" | awk -F'|' '{print $1}')
 host=$(echo "$result" | awk -F'|' '{print $2}')
 user=$(echo "$result" | awk -F'|' '{print $3}')
 
-if [[ "$BUTTON_URL" == "null" ]]; then
-  button_url="https://www.youtube.com/@frankiejun8965"
-else
-  button_url=${BUTTON_URL:-"https://www.youtube.com/@frankiejun8965"}
-fi
 
 URL="https://sctapi.ftqq.com/$sendKey.send?"
 
-if [[ -n "$host" ]]; then
-  button_url=$(replaceValue $button_url HOST $host)
-fi
-if [[ -n "$user" ]]; then
-  button_url=$(replaceValue $button_url USER $user)
-fi
-if [[ -n "$PASS" ]]; then
-  pass=$(toBase64 $PASS)
-  button_url=$(replaceValue $button_url PASS $pass)
-fi
-encoded_url=$(urlencode "$button_url")
-#echo "encoded_url: $encoded_url"
-reply_markup='{
-    "inline_keyboard": [
-      [
-        {"text": "点击查看", "url": "'"${encoded_url}"'"}
-      ]
-    ]
-  }'
-#echo "reply_markup: $reply_markup"
 
-res=$(timeout 20s curl -s -X POST https://5742.push.ft07.com/send/sctp5742t93u1b5cumq82fnkmdleu7p.send? -d desp=${formatted_msg} -d reply_markup="${reply_markup}")
+
+res=$(timeout 20s curl -s -X POST https://5742.push.ft07.com/send/sctp5742t93u1b5cumq82fnkmdleu7p.send? -d desp=${formatted_msg}")
   if [ $? == 124 ]; then
     echo "发送消息超时"
     exit 1
